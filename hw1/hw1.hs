9b029c0b5200c534114fe40e6c1f72f528644ab1
Sean Penney <penneys@os-class.engr.oregonstate.edu>
2014-04-20 21:32:59 -0700

    Worked on hw1

--- hw1/hw1.hs
+++ hw1/hw1.hs
@@ -3,27 +3,67 @@
 
 --module hw1 where
 
+import Prelude hiding (Num)
+
 -- Exercise 1, Mini Logo
 
---data Cmd = Pen Mode
---		| Moveto Pos Pos
---		| Def Name Pars Cmd
---		| Call Name Vals
-		| Commands [Cmd]
+--a)
+data Cmd = Pen Mode
+		| Moveto Pos Pos
+		| Def Name Pars Cmd
+		| Call Name Vals
+		| Seq [Cmd]
 		
---data Mode = Up | Down
+		
+data Mode = Up | Down
 
---data Pos = Num | Name
+data Pos = Foo Num | Bar Name
 
---data Pars = [Name]
+type Pars = [Name]
 
---data vals = [Num]
+type Vals = [Num]
 
---type Name = String
+type Name = String
 
---type Num = Int
+type Num = Int
 
+--b)
+vector = Def "vector" ["x1", "y1", "x2", "y2"] (Seq [Pen Up, Moveto (Bar "x1") (Bar "y1"), Pen Down, Moveto (Bar "x2") (Bar "y2")])
 
+--c)
+steps :: Int ->Cmd
+steps n | n < 1 = []
+		| n == 1 = Seq [Call "vector" [0, 0, 0, 1], Call "vector" [0, 1, 1, 1]] -- move from (0,0) to (1,1)
+		| n > 1 = Seq [steps (n-1), Seq[Call "vector" [n-1, n-1, n-1, n], Call "vector" [n-1, n, n, n]]]
+
+-- Exercsie 2, Digital Circuit Design Language
+
+--a)
+data Circuit = Circuit Gates Links
+
+data Gates = Gate Int Gatefn Gates | Empty
+
+data Gatefn = And | Or | Xor | Not
+				deriving Show
+
+data Links = Link Int Int Int Int links | Empty
+
+--b)
+Circuit (Gate 1 Xor (Gate 2 And Empty)) (Link 1 1 2 1 (Link 1 2 2 2 Empty))
+
+--c)
+ppCircuit :: Circuit -> String
+ppCircuit (Circuit g l) = ppGates g ++ ppLinks l
+
+ppGates :: Gates -> String
+ppGates Empty = ""
+ppGates (n fn g) = show n ++ ":" ++ show fn ++ ";\n" ++ pp g
+
+ppLinks :: Links -> String
+ppLinks Empty == ""
+ppLinks (a b c d l) = "from " ++ show a ++ "." ++ show b ++ " to " ++ show c ++ "." show d ++ ppLinks l
+
+		
 -- Exercise 3, Designing Abstract Syntax
 
 data Expr = N Int
@@ -43,9 +83,11 @@ result = Apply Multiply [Apply Negate[Apply Add[Num 3, Num 4]], Num 7]
 --The first representation only requires one constructor, which makes it easier to read.
 
 --In the first representation, one would be limited to using two numbers in the Plus and Times functions.
---In the second representation,
+--In the second representation, any amount of numbers could be used.
 
 --c)
-translate :: Expr -> Exxp
+translate :: Expr -> Exp
 translate(N x) = Num x
 translate(Plus x y) = Apply Add[translate(x), translate(y)]
+translate(Times x y) = Apply Multiply[translate(x), translate(y)]
+translate(Neg x) = Apply Negate [translate(x)]
