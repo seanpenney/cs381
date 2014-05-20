659a9186123f5b4cefa1bd4572d25551c5a53ef1
seanpenney <penneys@os-class.engr.oregonstate.edu>
2014-05-19 22:48:13 -0700

    Fixed up exercise 1 on hw4

--- hw4/Hw4.hs
+++ hw4/Hw4.hs
@@ -6,23 +6,24 @@
 --[]
 --[x:?]									push
 --[y:?, x:?]							push
---[f{}, y:?, x:?]						push
---[x:2, f{}, y:?, x:?]					push
---[x:1, x:2, f{}, y:?, x:?]				push
---[x:0, x:1, x:2, f{}, y:?, x:?]		push
+--[y:1, x:?]							push
+--[f{}, y:1, x:?]						push
+--[x:2, f{}, y:1, x:?]					push
+--[x:0, x:1, x:2, f{}, y:1, x:?]		push
+--[x:1, x:2, f{}, y:1, x:?]				push
 --[x:0, x:1, x:2, f{}, y:1, x:?]		push
 --[x:1, x:2, f{}, y:2, x:?]				pop
 --[x:2, f{}, y:4, x:?]					pop
---[f{}, y:?, x:4]						pop
---[y:?, x:4]							pop
+--[f{}, y:4, x:4]						pop
+--[y:4, x:4]							pop
 --[]									pop
 
 --Question 2
 
---a. If it is static, then the answer is 33.
+--a. If it is static, then the answer is 33
 --b. If it is dynamic, then the answer is 26
 
---Question 33
+--Question 3
 
 --a. y = 54, z = 112
 --b. y = -14, z = -28
